6f216dec878e0ce0de0953de10dbd1c8a5cfb95a
benjaminchume-droid <benjaminchume@gmail.com>
2026-09-10 08:32:53 +0100

    Delete web MainActivity

--- android/app/src/main/kotlin/com/pulseplayer/music/web/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-// Initialize web-based DRM
-val cookieManager = CookieManager(this)
-val webRegistry = WebDRMRegistry(OmniSourceRegistry(this), cookieManager)
-lifecycleScope.launch {
-    webRegistry.registerWebExtractors()
-    // Optionally, pre-authenticate if we have saved cookies
-}
